6551312f4a01b1491523bba55c8ed53d3625312f
Patrick Olusaba <patrick.olusaba@gmail.com>
2025-05-19 10:21:54 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './index.css'
+import ExpenseTracker from './components/ExpenseTracker'
+
+function App() {
+
+  return (
+    <div>
+      <ExpenseTracker/>
+    </div>
+  );
+};
+
+export default App
